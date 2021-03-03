bbc05a95c515fe872e8bd831280baafe7219d82d
KAttila98 <attilaka98@gmail.com>
2021-03-03 20:51:38 +0200

    Custom LSTM added

---
+++ LSTM/CustomLSTM.py
@@ -0,0 +1,124 @@
+import math
+import torch
+import torch.nn as nn
+
+
+class NaiveCustomLSTM(nn.Module):
+    def __init__(self, input_sz: int, hidden_sz: int):
+        super().__init__()
+        self.input_size = input_sz
+        self.hidden_size = hidden_sz
+
+        # i_t
+        self.U_i = nn.Parameter(torch.Tensor(input_sz, hidden_sz))
+        self.V_i = nn.Parameter(torch.Tensor(hidden_sz, hidden_sz))
+        self.b_i = nn.Parameter(torch.Tensor(hidden_sz))
+
+        # f_t
+        self.U_f = nn.Parameter(torch.Tensor(input_sz, hidden_sz))
+        self.V_f = nn.Parameter(torch.Tensor(hidden_sz, hidden_sz))
+        self.b_f = nn.Parameter(torch.Tensor(hidden_sz))
+
+        # c_t
+        self.U_c = nn.Parameter(torch.Tensor(input_sz, hidden_sz))
+        self.V_c = nn.Parameter(torch.Tensor(hidden_sz, hidden_sz))
+        self.b_c = nn.Parameter(torch.Tensor(hidden_sz))
+
+        # o_t
+        self.U_o = nn.Parameter(torch.Tensor(input_sz, hidden_sz))
+        self.V_o = nn.Parameter(torch.Tensor(hidden_sz, hidden_sz))
+        self.b_o = nn.Parameter(torch.Tensor(hidden_sz))
+
+        self.init_weights()
+
+    def init_weights(self):
+        stdv = 1.0 / math.sqrt(self.hidden_size)
+        for weight in self.parameters():
+            weight.data.uniform_(-stdv, stdv)
+
+    def forward(self, x, init_states=None):
+
+        """
+        x.shape: (batch_size, sequence_size, input_size)
+        """
+        bs, seq_sz, _ = x.size()
+        hidden_seq = []
+
+        if init_states is None:
+            h_t, c_t = (
+                torch.zeros(bs, self.hidden_size).to(x.device),
+                torch.zeros(bs, self.hidden_size).to(x.device),
+            )
+        else:
+            h_t, c_t = init_states
+
+        for t in range(seq_sz):
+            x_t = x[:, t, :]
+
+            i_t = torch.sigmoid(x_t @ self.U_i + h_t @ self.V_i + self.b_i)
+            f_t = torch.sigmoid(x_t @ self.U_f + h_t @ self.V_f + self.b_f)
+            g_t = torch.tanh(x_t @ self.U_c + h_t @ self.V_c + self.b_c)
+            o_t = torch.sigmoid(x_t @ self.U_o + h_t @ self.V_o + self.b_o)
+            c_t = f_t * c_t + i_t * g_t
+            h_t = o_t * torch.tanh(c_t)
+
+            # append a tensor with shape: [1, batch_size, hidden_size]
+            hidden_seq.append(h_t.unsqueeze(0))
+
+        # concatenate list to get a tensor with shape seq_sz x batch_size x hidden_size
+        hidden_seq = torch.cat(hidden_seq, dim=0)
+        # transpose tensor to get a shape of batch_size x seq_sz x hidden_size
+        hidden_seq = hidden_seq.transpose(0, 1).contiguous()
+        return hidden_seq, (h_t, c_t)
+
+
+class CustomLSTM(nn.Module):
+    def __init__(self, input_sz, hidden_sz, return_states = False, return_sequences = False):
+        super().__init__()
+        self.input_sz = input_sz
+        self.hidden_size = hidden_sz
+        self.return_states = return_states
+        self.return_sequences = return_sequences
+        self.W = nn.Parameter(torch.Tensor(input_sz, hidden_sz * 4))
+        self.U = nn.Parameter(torch.Tensor(hidden_sz, hidden_sz * 4))
+        self.bias = nn.Parameter(torch.Tensor(hidden_sz * 4))
+        self.init_weights()
+
+    def init_weights(self):
+        stdv = 1.0 / math.sqrt(self.hidden_size)
+        for weight in self.parameters():
+            weight.data.uniform_(-stdv, stdv)
+
+    def forward(self, x, init_states=None):
+
+        bs, seq_sz, _ = x.shape
+        hidden_seq = []
+        if init_states is None:
+            h_t, c_t = (torch.zeros(bs, self.hidden_size).to(x.device),
+                        torch.zeros(bs, self.hidden_size).to(x.device))
+        else:
+            h_t, c_t = init_states
+
+        HS = self.hidden_size
+        for t in range(seq_sz):
+            x_t = x[:, t, :]
+            # batch the computations into a single matrix multiplication
+            gates = x_t @ self.W + h_t @ self.U + self.bias
+            i_t, f_t, g_t, o_t = (
+                torch.sigmoid(gates[:, :HS]),  # input
+                torch.sigmoid(gates[:, HS:HS * 2]),  # forget
+                torch.tanh(gates[:, HS * 2:HS * 3]),
+                torch.sigmoid(gates[:, HS * 3:]),  # output
+            )
+            c_t = f_t * c_t + i_t * g_t
+            h_t = o_t * torch.tanh(c_t)
+            hidden_seq.append(h_t.unsqueeze(0))
+        hidden_seq = torch.cat(hidden_seq, dim=0)
+        # reshape from shape (sequence, batch, feature) to (batch, sequence, feature)
+        hidden_seq = hidden_seq.transpose(0, 1).contiguous()
+        if not self.return_sequences:
+            hidden_seq = hidden_seq[:,-1, :]
+        if self.return_states:
+            return hidden_seq, (h_t, c_t)
+        else:
+            return hidden_seq
